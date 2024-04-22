65b977659f2754cf094a4ff1e7f1dbc55633b8ed
Furqan27659 <142151271+Furqan27659@users.noreply.github.com>
2024-04-22 22:10:05 +0500

    final & const variables

---
+++ const&final.dart
@@ -0,0 +1,36 @@
+void main()
+{
+
+  //final varibale like name is nor reassignable
+  final name= "FUrqan";
+
+  const age = 11; // value not changeable and also initialise at that time
+
+
+  print("name $name");
+  print("age $age");
+
+  //Final variable
+  final names=[
+    "Furqan",
+    "Abdul-Rehman",
+    "Faheem"
+  ];
+   names.add("Supervisor");
+   print("Final list $names");
+
+
+   // CONST variable
+   // const ages = [
+   //   23,
+   //   25,
+   //   24
+   // ];
+   // ages.add(26); // we cant add anything while using const variale
+   // print(ages);
+
+
+
+
+
+}

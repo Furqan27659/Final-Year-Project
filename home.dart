0568ee96b2080113aeeda489532ba54ea7c227da
Furqan27659 <142151271+Furqan27659@users.noreply.github.com>
2024-05-28 04:31:48 +0500

    Home
    Adding sharedpref on logoutButton

--- home.dart
+++ home.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:fruit_mate/login.dart';
+import 'package:fruit_mate/splash_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
-  runApp(home());
+  runApp(Home());
 }
 
-class home extends StatelessWidget {
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -31,6 +34,10 @@ class HomePage extends StatelessWidget {
             ElevatedButton(
               onPressed: () {
                 // Navigation logic for Classification page
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ClassificationPage()),
+                );
               },
               child: Text('Classification'),
             ),
@@ -47,6 +54,51 @@ class HomePage extends StatelessWidget {
                 // Navigation logic for Shelf-life page
               },
               child: Text('Shelf-life'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () async{
+                var sharedPref = await SharedPreferences.getInstance();
+                sharedPref.setBool(SplashScreenState.KEYLOGIN, true);
+
+
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginPage()),
+                );
+
+                // Navigation logic for Fresh/Rotten page
+              },
+              child: Text('LogOut'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ClassificationPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Classification'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Tap on Scan to detect Fruit',
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Scan logic will go here
+              },
+              child: Text('Scan'),
             ),
           ],
         ),
